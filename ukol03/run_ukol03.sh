55961d00640a32968b03a700c0bda7e8b170a42c
Michal Stanke <stankmic@fel.cvut.cz>
2016-04-06 17:56:47 +0200

    Fix run ukol03

--- ukol03/run_ukol03.sh
+++ ukol03/run_ukol03.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 find ./2016-1-cflp-mathprog -iname '*.dat' -exec timeout 15 glpsol -m ukol03.mod -d "{}" -y "{}.txt" \;
-rename 's/dat.txt$/txt/' *.dat.txt
+rename 's/dat.txt$/txt/' 2016-1-cflp-mathprog/*.dat.txt
