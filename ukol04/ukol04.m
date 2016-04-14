20f711698bbead8234486b583b039f6b92ccad70
Michal Stanke <stankmic@fel.cvut.cz>
2016-04-14 08:13:42 +0200

    ukol04 fixed

--- ukol04/ukol04.m
+++ ukol04/ukol04.m
@@ -16,143 +16,19 @@ function ukol04()
             pairNum = pairNum+1;
             switch(char(pair))
                 case 'CR'
-                    [n1,n2,l,b] = common(sumC, sumR);
-                    tmpC = ones(n1,n2)-I';
-                    tmpU = ones(n1,n2);
-                    c = [zeros(n1,n1) tmpC;
-                        zeros(n2,n1) zeros(n2,n2)];
-                    u = [zeros(n1,n1) tmpU;
-                        zeros(n2,n1) zeros(n2,n2)];
-                    F = mincost(c,l,u,b);
-                    I = F(1:n1,n1+1:n1+n2)';
+                    I = pairCR(I, sumC, sumR);
                 case 'CA'
-                    [n1,n2,l,b] = common(sumC, sumA);
-                    tmpC = zeros(n1,n2);
-                    tmpU = zeros(n1,n2);
-                    for i=1:n1
-                        for j=i:i+n1-1
-                            k_ = j-i+1;
-                            l_ = i;
-                            tmpC(i,j) = 1 - I(k_,l_);
-                            tmpU(i,j) = 1;
-                        end
-                    end
-                    c = [zeros(n1,n1) tmpC;
-                        zeros(n2,n1) zeros(n2,n2)];
-                    u = [zeros(n1,n1) tmpU;
-                        zeros(n2,n1) zeros(n2,n2)];
-                    F = mincost(c,l,u,b);
-                    for i=1:n1
-                        for j=i:i+n1-1
-                            k_ = j-i+1;
-                            l_ = i;
-                            I(k_,l_) = F(i,j+n1);
-                        end
-                    end
+                    I = pairCA(I, sumC, sumA);
                 case 'CD'
-                    [n1,n2,l,b] = common(sumC, sumD);
-                    tmpC = zeros(n1,n2);
-                    tmpU = zeros(n1,n2);
-                    for i=1:n1
-                        for j=i:i+n1-1
-                            k_ = n1-(j-i);
-                            l_ = i;
-                            tmpC(i,j) = 1 - I(k_,l_);
-                            tmpU(i,j) = 1;
-                        end
-                    end
-                    c = [zeros(n1,n1) tmpC;
-                        zeros(n2,n1) zeros(n2,n2)];
-                    u = [zeros(n1,n1) tmpU;
-                        zeros(n2,n1) zeros(n2,n2)];
-                    F = mincost(c,l,u,b);
-                    for i=1:n1
-                        for j=i:i+n1-1
-                            k_ = n1-(j-i);
-                            l_ = i;
-                            I(k_,l_) = F(i,j+n1);
-                        end
-                    end
+                    I = pairCD(I, sumC, sumD);
                 case 'RA'
-                    [n1,n2,l,b] = common(sumR, sumA);
-                    tmpC = zeros(n1,n2);
-                    tmpU = zeros(n1,n2);
-                    for i=1:n1
-                        for j=i:i+n1-1
-                            k_ = i;
-                            l_ = j-(i-1);
-                            tmpC(i,j) = 1 - I(k_,l_);
-                            tmpU(i,j) = 1;
-                        end
-                    end
-                    c = [zeros(n1,n1) tmpC;
-                        zeros(n2,n1) zeros(n2,n2)];
-                    u = [zeros(n1,n1) tmpU;
-                        zeros(n2,n1) zeros(n2,n2)];
-                    F = mincost(c,l,u,b);
-                    for i=1:n1
-                        for j=i:i+n1-1
-                            k_ = i;
-                            l_ = j-(i-1);
-                            I(k_,l_) = F(i,j+n1);
-                        end
-                    end
+                    I = pairRA(I, sumR, sumA);
                 case 'RD'
-                    [n1,n2,l,b] = common(sumR, sumD);
-                    tmpC = zeros(n1,n2);
-                    tmpU = zeros(n1,n2);
-                    c = [zeros(n1,n1) zeros(n1,n2);
-                        zeros(n2,n1) zeros(n2,n2)];
-                    u = [zeros(n1,n1) zeros(n1,n2);
-                        zeros(n2,n1) zeros(n2,n2)];
-                    for i=1:n1
-                        for j=i:i+n1-1
-                            k_ = i;
-                            l_ = j-(i-1);
-                            c(i,j-2*(i-1)+n2) = 1 - I(k_,l_);
-                            u(i,j-2*(i-1)+n2) = 1;
-                        end
-                    end
-                    F = mincost(c,l,u,b);
-                    for i=1:n1
-                        for j=i:i+n1-1
-                            k_ = i;
-                            l_ = j-(i-1);
-                            I(k_,l_) = F(i,j-2*(i-1)+n2);
-                        end
-                    end
+                    I = pairRD(I, sumR, sumD);
                 case 'AD'
-                    [n1,n2,l,b] = common(sumD, sumA);
-                    [~,imgN] = size(sumR);
-                    tmpC = zeros(n1,n2);
-                    tmpU = zeros(n1,n2);
-                    c = [zeros(n1,n1) tmpC;
-                        zeros(n2,n1) zeros(n2,n2)];
-                    u = [zeros(n1,n1) tmpU;
-                        zeros(n2,n1) zeros(n2,n2)];
-                    for k_=1:imgN
-                        for l_=1:imgN
-                            i = imgN-k_+l_;
-                            j = k_+l_+n1-1;
-                            c(i,j) = 1 - I(k_,l_);
-                            u(i,j) = 1;
-                        end
-                    end
-                    F = mincost(c,l,u,b);
-                    for k_=1:imgN
-                        for l_=1:imgN
-                            i = imgN-k_+l_;
-                            j = k_+l_+n1-1;
-                            I(k_,l_) = F(i,j);
-                        end
-                    end
+                    I = pairAD(I, sumA, sumD, sumR);
             end
-            subplot(countOfIterations,numOfPairs,numOfPairs*(ic-1)+pairNum);
-            imagesc(logical(I));
-            colormap(gray);
-            axis off;
-            axis square;
-            drawnow;
+            updateFigure(I, countOfIterations, numOfPairs, numOfPairs*(ic-1)+pairNum);
         end
         if isequal(I,lastI)
             disp(ic)
@@ -170,7 +46,159 @@ function [n1,n2,l,b] = common(sumP1,sumP2)
     b = [sumP1 -sumP2]';
 end
 
+function I = pairCR(I, sumC, sumR)
+    [n1,n2,l,b] = common(sumC, sumR);
+    tmpC = ones(n1,n2)-I';
+    tmpU = ones(n1,n2);
+    c = [zeros(n1,n1) tmpC;
+        zeros(n2,n1) zeros(n2,n2)];
+    u = [zeros(n1,n1) tmpU;
+        zeros(n2,n1) zeros(n2,n2)];
+    F = mincost(c,l,u,b);
+    I = F(1:n1,n1+1:n1+n2)';
+end
+
+function I = pairCA(I, sumC, sumA)
+    [n1,n2,l,b] = common(sumC, sumA);
+    tmpC = zeros(n1,n2);
+    tmpU = zeros(n1,n2);
+    for i=1:n1
+        for j=i:i+n1-1
+            k_ = j-i+1;
+            l_ = i;
+            tmpC(i,j) = 1 - I(k_,l_);
+            tmpU(i,j) = 1;
+        end
+    end
+    c = [zeros(n1,n1) tmpC;
+        zeros(n2,n1) zeros(n2,n2)];
+    u = [zeros(n1,n1) tmpU;
+        zeros(n2,n1) zeros(n2,n2)];
+    F = mincost(c,l,u,b);
+    for i=1:n1
+        for j=i:i+n1-1
+            k_ = j-i+1;
+            l_ = i;
+            I(k_,l_) = F(i,j+n1);
+        end
+    end
+end
+
+function I = pairCD(I, sumC, sumD)
+    [n1,n2,l,b] = common(sumC, sumD);
+    tmpC = zeros(n1,n2);
+    tmpU = zeros(n1,n2);
+    for i=1:n1
+        for j=i:i+n1-1
+            k_ = n1-(j-i);
+            l_ = i;
+            tmpC(i,j) = 1 - I(k_,l_);
+            tmpU(i,j) = 1;
+        end
+    end
+    c = [zeros(n1,n1) tmpC;
+        zeros(n2,n1) zeros(n2,n2)];
+    u = [zeros(n1,n1) tmpU;
+        zeros(n2,n1) zeros(n2,n2)];
+    F = mincost(c,l,u,b);
+    for i=1:n1
+        for j=i:i+n1-1
+            k_ = n1-(j-i);
+            l_ = i;
+            I(k_,l_) = F(i,j+n1);
+        end
+    end
+end
+
+function I = pairRA(I, sumR, sumA)
+    [n1,n2,l,b] = common(sumR, sumA);
+    tmpC = zeros(n1,n2);
+    tmpU = zeros(n1,n2);
+    for i=1:n1
+        for j=i:i+n1-1
+            k_ = i;
+            l_ = j-(i-1);
+            tmpC(i,j) = 1 - I(k_,l_);
+            tmpU(i,j) = 1;
+        end
+    end
+    c = [zeros(n1,n1) tmpC;
+        zeros(n2,n1) zeros(n2,n2)];
+    u = [zeros(n1,n1) tmpU;
+        zeros(n2,n1) zeros(n2,n2)];
+    F = mincost(c,l,u,b);
+    for i=1:n1
+        for j=i:i+n1-1
+            k_ = i;
+            l_ = j-(i-1);
+            I(k_,l_) = F(i,j+n1);
+        end
+    end
+end
+
+function I = pairRD(I, sumR, sumD)
+    [n1,n2,l,b] = common(sumR, sumD);
+    tmpC = zeros(n1,n2);
+    tmpU = zeros(n1,n2);
+    c = [zeros(n1,n1) tmpC;
+        zeros(n2,n1) zeros(n2,n2)];
+    u = [zeros(n1,n1) tmpU;
+        zeros(n2,n1) zeros(n2,n2)];
+    for i=1:n1
+        for j=i:i+n1-1
+            k_ = i;
+            l_ = j-(i-1);
+            c(i,j-2*(i-1)+n2) = 1 - I(k_,l_);
+            u(i,j-2*(i-1)+n2) = 1;
+        end
+    end
+    F = mincost(c,l,u,b);
+    for i=1:n1
+        for j=i:i+n1-1
+            k_ = i;
+            l_ = j-(i-1);
+            I(k_,l_) = F(i,j-2*(i-1)+n2);
+        end
+    end
+end
+
+function I = pairAD(I, sumA, sumD, sumR)
+    [n1,n2,l,b] = common(sumD, sumA);
+    [~,imgN] = size(sumR);
+    tmpC = zeros(n1,n2);
+    tmpU = zeros(n1,n2);
+    c = [zeros(n1,n1) tmpC;
+        zeros(n2,n1) zeros(n2,n2)];
+    u = [zeros(n1,n1) tmpU;
+        zeros(n2,n1) zeros(n2,n2)];
+    for k_=1:imgN
+        for l_=1:imgN
+            i = imgN-k_+l_;
+            j = k_+l_+n1-1;
+            c(i,j) = 1 - I(k_,l_);
+            u(i,j) = 1;
+        end
+    end
+    F = mincost(c,l,u,b);
+    for k_=1:imgN
+        for l_=1:imgN
+            i = imgN-k_+l_;
+            j = k_+l_+n1-1;
+            I(k_,l_) = F(i,j);
+        end
+    end
+end
+
 function F = mincost(c,l,u,b)
     G = graph;
     F = G.mincostflow(c,l,u,b);
+end
+
+function updateFigure(I, rows, cols, order)
+    subplot(rows,cols,order);
+    imagesc(logical(I));
+	colormap(gray);
+	axis off;
+    axis square;
+	drawnow;
 end
